f7aef4d90eecd6aa9ef4849c93fcf8305f62b98d
TAYOKEN <yanisyaya322@gmail.com>
2025-06-05 16:35:04 +0200

    NEW PAGES

--- phpunesco/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();  // Démarre ou reprend la session
-session_destroy();  // Détruit la session
-header('Location: login.php');  // Redirige l'utilisateur vers la page de connexion
-exit();
-?>
